92c2c2a3c2e9bdf322afe8ba643af3282e9c4e6a
TummusInc <kootblad@live.nl>
2022-03-07 13:27:03 +0100

    Echte echte commit

---
+++ huisjes.php
@@ -0,0 +1,21 @@
+<?php 
+include "includes/database.php";
+include "header.php";
+
+
+$huisjeid = 1;
+if ($huisjeid < 1 ){
+    die()
+}
+
+
+// try {
+//     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+//      // set the PDO error mode to exception
+//      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//        echo "Connected successfully";
+//    } catch(PDOException $err) {
+//        echo "Connection failed: " . $err->getMessage();
+//    }
+?>
+
